687226b59a37954d8bbfdc0be254412b0e4ff91f
lotapp <39723758+lotapp@users.noreply.github.com>
2018-06-11 12:56:45 +0800

    add set and ext online demo

--- python/1.POP/3.list_tuple_dict/4.set_and_ext.py
+++ python/1.POP/3.list_tuple_dict/4.set_and_ext.py
@@ -1,3 +1,33 @@
+# set
+# set和dict类似，也是一组key的集合，但不存储value。由于key不能重复，so，在set中，没有重复的值
+# help(set)
+#   #set(iterable) -> new set object #列表就比较合适了
+my_set=set([1,2,1,"mmd"])
+# 遍历 my_set
+for item in my_set:
+    print(item)
+# 添加元素
+my_set.add("add") #没有返回值
+print(my_set)
+
+# 删除元素
+my_set.remove("mmd") #没有返回值
+print(my_set)
+# my_set.remove("mmd") #不存在则报错
+
+#利用运算符+set 实现数学方面的扩展
+set1=set([1,2,5])
+set2=set([2,4,6])
+
+#交集
+print(set1 & set2)
+
+#并集
+print(set1 | set2)
+
+# 其他内容可以直接查看help
+# ----------------------------------
+
 # test_str="www.baidu.com"
 # test_list=[1,"d",5]
 # test_dict={"name":"dnt","wechat":"xxx"}
@@ -36,13 +66,13 @@
 # # max(item)	        返回容器中元素最大值
 # max(test_str)
 # test_list=[1,3,5,7,9,2]
-# max(test_list) #TypeError: '>' not supported between instances of 'str' and 'int'
+# max(test_list) 
 # max(test_dict) #比较key
 
-# # min(item)	        返回容器中元素最小值
-# min(test_str)
-# min(test_list)
-# min(test_dict)
+# # min(item) 返回容器中元素最小值
+# print(min(test_str))
+# print(min(test_list))
+# print(min(test_dict))
 
 # # del(item)	        删除变量
 # # del() or del xxx
