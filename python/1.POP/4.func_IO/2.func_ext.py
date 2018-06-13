f5bc3a1ed631c029501f123d82b8b69d143068b8
lotapp <39723758+lotapp@users.noreply.github.com>
2018-06-13 13:23:02 +0800

    demo 到位

--- python/1.POP/4.func_IO/2.func_ext.py
+++ python/1.POP/4.func_IO/2.func_ext.py
@@ -45,46 +45,56 @@ print(data_list)
 data_list.sort(key=lambda x:x["name"])
 print(data_list)
 
+# 当函数的参数传递(有时候需要传一个匿名方法去函数中处理某些事情)
 def sum(a,b,func):
    return func(a,b)
 
 print(sum(1,2,lambda x,y: x+y))
+
+# ##############################################################
+# # 可变类型全局变量，不可变型全局变量：
+a=100
+
+def set_num(num):
+    a=120
+
+print(a)
+set_num(a)
+print(a)
+
+# 在函数中不使用global声明全局变量不能修改全局变量==>本质是不能修改全局变量的指向，即不能将全局变量指向新的数据
+# 对于不可变类型的全局变量来说，因其指向的数据不能修改，所以不使用global时无法修改全局变量
+# 对于可变类型的全局变量来说，因其指向的数据可以修改，所以不使用global时也可修改全局变量
+
 ####################### 验证系列 ###################################
 # 自摸索阶段:(之前很多资料说不能调用print函数,自己测试下)
 # 定义一个无参匿名函数
 printf=lambda:print("I Love You")
 printf()
 
-# # 可变类型与不可变类型的变量分别作为函数参数时
-# #
-# def default_some_params(nums):
-#     pass
+# 可变类型与不可变类型的变量分别作为函数参数时
+# 感到疑惑可以看之前的运算符扩展（https://www.cnblogs.com/dotnetcrazy/p/9155310.html#ext）
+def default_some_params(nums):
+    nums+=nums
 
-# test_list=list(range(1,6))
-# default_some_params(test_list)
-# print(test_list)
+test_num=1
+default_some_params(test_num)
+print(test_num)
+
+test_list=list(range(1,6))
+default_some_params(test_list)
+print(test_list)
 
 # # Python中函数参数是引用传递。对于不可变类型，因变量不能修改，所以运算不会影响到变量自身；而对于可变类型来说，函数体中的运算有可能会更改传入的参数变量。
 
 # ##############################################################
+# 函数名能不能重复的问题（能不能重载：具有不同的参数的类型或参数的个数）
+def test():
+    pass
 
-# # 可变类型全局变量，不可变型全局变量：
+def test(a,b):
+    return a+b
 
-
-# # 在函数中不使用global声明全局变量不能修改全局变量==>本质是不能修改全局变量的指向，即不能将全局变量指向新的数据
-# # 对于不可变类型的全局变量来说，因其指向的数据不能修改，所以不使用global时无法修改全局变量
-# # 对于可变类型的全局变量来说，因其指向的数据可以修改，所以不使用global时也可修改全局变量
-
-# ##############################################################
-
-# # 函数名不能重复
-# def test():
-#     pass
-
-# def test(a,b):
-#     return a+b
-# #TypeError: test() missing 2 required positional arguments: 'a' and 'b'
+test(1,2)
+# # TypeError: test() missing 2 required positional arguments: 'a' and 'b'
 # test() #前一个直接被后一个覆盖掉了
-
-# 函数可以直接赋值给变量,然后直接用
-# 来个测试:
