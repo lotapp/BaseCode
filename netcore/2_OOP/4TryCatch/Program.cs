0a07540efbe5f1003806f0bafe83236acacaa2c4
lotapp <39723758+lotapp@users.noreply.github.com>
2018-06-24 10:44:16 +0800

    C#异常的案例

---
+++ netcore/2_OOP/4TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("出错了啊");
+                //Convert.ToInt32("mmd");
+            }
+            catch (Exception ex)
+            {
+                // Input string was not in a correct format
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+        }
+    }
+}
