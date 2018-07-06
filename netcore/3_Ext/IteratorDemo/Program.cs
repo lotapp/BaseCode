9bd87c134f0ffe625cd485f93ab1d2d4be81fc74
lotapp <39723758+lotapp@users.noreply.github.com>
2018-07-06 21:48:32 +0800

    yield demo

---
+++ netcore/3_Ext/IteratorDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 枚举器遍历
+            var tmp = FibonaByIEnumerator(30);
+            while (tmp.MoveNext())
+            {
+                Console.WriteLine(tmp.Current);
+            }
+            // 可枚举类型遍历
+            foreach (var item in FibonaByIEnumerable(30))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// 返回一个枚举器
+        /// </summary>
+        public static IEnumerator<int> FibonaByIEnumerator(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                yield return b;
+                (a, b) = (b, a + b);
+            }
+        }
+
+        /// <summary>
+        /// 返回一个可枚举类型
+        /// </summary>
+        public static IEnumerable<int> FibonaByIEnumerable(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                yield return b;
+                (a, b) = (b, a + b);
+            }
+        }
+    }
+}
