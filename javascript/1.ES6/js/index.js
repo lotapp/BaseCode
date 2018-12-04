9a6d68ece5fd71f0877888651da290af33586b21
unknown <39723758+lotapp@users.noreply.github.com>
2018-12-04 21:40:27 +0800

    add mod demo

---
+++ javascript/1.ES6/js/index.js
@@ -0,0 +1,4 @@
+import { a, test, user } from ".mod.js"
+console.log(a);
+test();
+console.log(user());
---
+++ javascript/1.ES6/js/mod.js
@@ -0,0 +1,12 @@
+export { a, test, user }
+
+let test = () => {
+    console.log("test");
+}
+
+let user = () => {    
+    console.log("user");
+    return "小明";
+}
+
+let a = 1;
