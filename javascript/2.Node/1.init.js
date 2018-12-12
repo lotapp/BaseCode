191419fe17c810cbed768b82631673ba331e04a4
unknown <39723758+lotapp@users.noreply.github.com>
2018-12-12 11:19:10 +0800

    nodejs init demo

---
+++ javascript/2.Node/1.init.js
@@ -0,0 +1,13 @@
+// import http from "http";
+const http = require("http")
+
+let server = http.createServer((request, response) => {
+    // 每次请求都会执行这个方法
+    console.log(request.url);
+
+    response.write("<h1>Test NodeJS</h>");
+    response.end() // 告诉浏览器响应结束
+
+});
+
+server.listen(8080); // 这个和其他语言不一样，直接监听对应的端口
