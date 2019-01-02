d3ffd6f6706b74265f70997a77a43a510c8b97af
unknown <39723758+lotapp@users.noreply.github.com>
2019-01-02 10:11:34 +0800

    add some demo for asyncio

---
+++ python/5.concurrent/ZCoroutine/2.asyncio_loop2.py
@@ -0,0 +1,27 @@
+import asyncio
+
+# 模拟一个耗时操作
+async def test(i):
+    print("start...")
+    # 不能再使用以前阻塞的暂停了
+    await asyncio.sleep(2)
+    print("end...")
+    return i
+
+
+if __name__ == '__main__':
+    import time
+
+    start_time = time.time()
+
+    tasks = [test(i) for i in range(10)]
+
+    # >=python3.4
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+
+    # python3.7
+    # result_list = asyncio.gather(*tasks)
+    # print(result_list)
+
+    print(time.time() - start_time)
