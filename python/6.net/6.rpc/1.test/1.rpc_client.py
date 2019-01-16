a32f8a1e4be8995da5ebafffb24c1e631322c5a2
unknown <39723758+lotapp@users.noreply.github.com>
2019-01-16 12:03:46 +0800

    rname

--- python/6.net/6.rpc/1.test/1.rpc_client.py
+++ python/6.net/6.rpc/1.test/1.rpc_client.py
@@ -1,5 +1,5 @@
 from xmlrpc.client import ServerProxy
 
-server = ServerProxy("http://192.168.36.123:50051")
-result = server.sum(1, 2)
+stub = ServerProxy("http://192.168.36.123:50051")
+result = stub.sum(1, 2)
 print(f"1+2={result}")
