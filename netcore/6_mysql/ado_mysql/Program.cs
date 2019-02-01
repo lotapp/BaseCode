766c2a5ba85bebd6f98f50a9b0f8da7bbf8fb380
lotapp <39723758+lotapp@users.noreply.github.com>
2019-02-01 13:44:25 +0800

    add mysql demo

---
+++ netcore/6_mysql/ado_mysql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ado_mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
