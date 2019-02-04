b23825b5d2c661fdebc4a7bfe10b6d957575fc7c
unknown <39723758+lotapp@users.noreply.github.com>
2019-02-04 11:27:25 +0800

    init mysql netcore demo

--- netcore/6_mysql/ado_mysql/Program.cs
+++ netcore/6_mysql/ado_mysql/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace ado_mysql
 {
@@ -6,7 +8,15 @@ namespace ado_mysql
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Data Source：ip地址，Initial Catalog：数据库名，User ID：用户名，Password：密码，port：端口
+            string connStr = "Data Source=192.168.36.144;Initial Catalog=worktemp;User ID=root;Password=dntdnt;CharSet=utf8;port=3306";
+            using (var conn = new MySqlConnection(connStr))
+            {
+                conn.Open(); //打开连接
+                var result = conn.QueryFirst("select count(*) from userinfo;");
+                System.Console.WriteLine(result);
+            }
+            Console.WriteLine("done...");
         }
     }
 }
