a413368fbd37f153a3ef76421d50cb770b239252
unknown <39723758+lotapp@users.noreply.github.com>
2019-02-04 11:27:01 +0800

    add condition

--- python/5.concurrent/ZCoroutine/z_new_ipc/8.condition.py
+++ python/5.concurrent/ZCoroutine/z_new_ipc/8.condition.py
@@ -1,31 +1,44 @@
 import asyncio
 
 cond = None
+p_list = []
 
 
-def div_init():
-    # do something
-    return True
+# 生产者
+async def producer(n):
+    for i in range(5):
+        async with cond:
+            p_list.append(f"{n}-{i}")
+            print(f"[生产者{n}]生产商品{n}-{i}")
+            # 通知任意一个消费者
+            cond.notify()  # 通知全部消费者：cond.notify_all()
+        # 摸拟一个耗时操作
+        await asyncio.sleep(0.01)
 
 
-# 等待普通方法func返回ture后继续执行
-async def test(func):
-    async with cond:
-        await cond.wait_for(func)
-        print("do something...")
+# 消费者
+async def consumer(i):
+    while True:
+        async with cond:
+            if p_list:
+                print(f"列表商品：{p_list}")
+                name = p_list.pop()  # 消费商品
+                print(f"[消费者{i}]消费商品{name}")
+                print(f"列表剩余：{p_list}")
+
+                # 摸拟一个耗时操作
+                await asyncio.sleep(0.01)
+            else:
+                await cond.wait()
 
 
 async def main():
     global cond
     cond = asyncio.Condition()  # 初始化condition
-
-    await asyncio.gather(test(div_init))
+    p_tasks = [asyncio.create_task(producer(i)) for i in range(2)]  # 两个生产者
+    c_tasks = [asyncio.create_task(consumer(i)) for i in range(5)]  # 五个消费者
+    await asyncio.gather(*p_tasks, *c_tasks)
 
 
 if __name__ == "__main__":
-    import time
-    start_time = time.time()
-
     asyncio.run(main())
-
-    print(time.time() - start_time)
