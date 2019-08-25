bbaac1f953e4d626b6c4703b655b9fd26c4f8292
unknown <39723758+lotapp@users.noreply.github.com>
2019-08-25 09:55:19 +0800

    before udf

---
+++ safe/第04周：系统函数/Code/index.php
@@ -0,0 +1,28 @@
+﻿<?php
+    // 自己构造的一个sqli页面
+    $pms=$_GET["id"];
+    if(empty($pms)){
+        $pms=1;
+    }
+    echo "id=" . $pms . "<br/>";
+
+    $conn=new mysqli("localhost", "用户名", "密码", "数据库");
+    if ($conn->connect_error) {
+        die("连接失败：" . $conn->connect_error);
+    }
+
+    // 防止中文乱码
+    $conn->query("set names utf8;");
+    $sql = "select username,password from workdb.users where id=" . $pms;
+    echo "SQL：" . $sql . "<br/>";
+
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            echo "username：" . $row["username"] . "，name：" . $row["password"] . "<br/>";
+        }
+    } else {
+        echo "no results";
+    }
+    $conn->close();
+?>
